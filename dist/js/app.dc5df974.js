b2c1c96f0b3546ce0a4c5108b2c595bfab2b3794
HanByul Jang <j.1star.0726@gmail.com>
2019-10-11 09:12:50 +0900

    [Add] chrome extension dist
    add chrome extension dist

---
+++ dist/js/app.dc5df974.js
@@ -0,0 +1,130 @@
+﻿(function (t) {
+	function e(e) {
+		for (var r, c, i = e[0], s = e[1], u = e[2], d = 0, p = []; d < i.length; d++) c = i[d], Object.prototype.hasOwnProperty.call(o, c) && o[c] && p.push(o[c][0]), o[c] = 0;
+		for (r in s) Object.prototype.hasOwnProperty.call(s, r) && (t[r] = s[r]);
+		l && l(e);
+		while (p.length) p.shift()();
+		return a.push.apply(a, u || []), n()
+	}
+
+	function n() {
+		for (var t, e = 0; e < a.length; e++) {
+			for (var n = a[e], r = !0, i = 1; i < n.length; i++) {
+				var s = n[i];
+				0 !== o[s] && (r = !1)
+			}
+			r && (a.splice(e--, 1), t = c(c.s = n[0]))
+		}
+		return t
+	}
+
+	var r = {}, o = {app: 0}, a = [];
+
+	function c(e) {
+		if (r[e]) return r[e].exports;
+		var n = r[e] = {i: e, l: !1, exports: {}};
+		return t[e].call(n.exports, n, n.exports, c), n.l = !0, n.exports
+	}
+
+	c.m = t, c.c = r, c.d = function (t, e, n) {
+		c.o(t, e) || Object.defineProperty(t, e, {enumerable: !0, get: n})
+	}, c.r = function (t) {
+		"undefined" !== typeof Symbol && Symbol.toStringTag && Object.defineProperty(t, Symbol.toStringTag, {value: "Module"}), Object.defineProperty(t, "__esModule", {value: !0})
+	}, c.t = function (t, e) {
+		if (1 & e && (t = c(t)), 8 & e) return t;
+		if (4 & e && "object" === typeof t && t && t.__esModule) return t;
+		var n = Object.create(null);
+		if (c.r(n), Object.defineProperty(n, "default", {
+			enumerable: !0,
+			value: t
+		}), 2 & e && "string" != typeof t) for (var r in t) c.d(n, r, function (e) {
+			return t[e]
+		}.bind(null, r));
+		return n
+	}, c.n = function (t) {
+		var e = t && t.__esModule ? function () {
+			return t["default"]
+		} : function () {
+			return t
+		};
+		return c.d(e, "a", e), e
+	}, c.o = function (t, e) {
+		return Object.prototype.hasOwnProperty.call(t, e)
+	}, c.p = "/";
+	var i = window["webpackJsonp"] = window["webpackJsonp"] || [], s = i.push.bind(i);
+	i.push = e, i = i.slice();
+	for (var u = 0; u < i.length; u++) e(i[u]);
+	var l = s;
+	a.push([0, "chunk-vendors"]), n()
+})({
+	0: function (t, e, n) {
+		t.exports = n("56d7")
+	}, "034f": function (t, e, n) {
+		"use strict";
+		var r = n("64a9"), o = n.n(r);
+		o.a
+	}, 3021: function (t, e, n) {
+		"use strict";
+		var r = n("94f0"), o = n.n(r);
+		o.a
+	}, "56d7": function (t, e, n) {
+		"use strict";
+		n.r(e);
+		n("cadf"), n("551c"), n("f751"), n("097d");
+		var r = n("2b0e"), o = function () {
+				var t = this, e = t.$createElement, n = t._self._c || e;
+				return n("div", {attrs: {id: "app"}}, [n("SSAFYChatBot")], 1)
+			}, a = [], c = function () {
+				var t = this, e = t.$createElement, n = t._self._c || e;
+				return n("div", {
+					class: {open: t.isActive},
+					attrs: {id: "chatbot-container"},
+					on: {click: t.openChatScreen}
+				}, [n("div", {
+					attrs: {id: "close-btn"},
+					on: {click: t.closeChatScreen}
+				}), n("div", {attrs: {id: "chatbot-header"}}, [t._v("SSAFY BOT")]), t._m(0), n("div", {attrs: {id: "chatbot-footer"}}, [n("input", {
+					directives: [{
+						name: "model",
+						rawName: "v-model",
+						value: t.message,
+						expression: "message"
+					}], attrs: {id: "user-chat"}, domProps: {value: t.message}, on: {
+						keydown: function (e) {
+							return !e.type.indexOf("key") && t._k(e.keyCode, "enter", 13, e.key, "Enter") ? null : t.sendChat(e)
+						}, input: function (e) {
+							e.target.composing || (t.message = e.target.value)
+						}
+					}
+				})])])
+			}, i = [function () {
+				var t = this, e = t.$createElement, n = t._self._c || e;
+				return n("div", {attrs: {id: "chatbot-body"}}, [n("div", {staticClass: "bot"}, [n("div", {staticClass: "thumbnail"}), n("div", {staticClass: "chat-wrapper"}, [t._v("\n\t\t\t\t안녕하세요. SSAFY BOT입니다. 저에게 말을 걸어주세요.\n\t\t\t")])])])
+			}], s = {
+				name: "SSAFYChatBot", props: {}, data: function () {
+					return {isActive: !1, message: ""}
+				}, mounted: function () {
+				}, methods: {
+					openChatScreen: function () {
+						this.$el.classList.contains("open") || (this.isActive = !0)
+					}, closeChatScreen: function () {
+						event.stopPropagation(), this.isActive = !1
+					}, sendChat: function () {
+						this.$http.get("http://j1star.ddns.net:8080/chat?q=" + this.message).then((function (t) {
+							document.querySelector("#chatbot-body").innerHTML += '<div class="bot">\n\t\t\t\t<div class="thumbnail"></div>\n\t\t\t\t<div class="chat-wrapper">' + t.data + "</div>\n\t\t\t</div>", document.querySelector("#chatbot-body").scrollTop = document.querySelector("#chatbot-body").scrollHeight
+						})), document.querySelector("#chatbot-body").innerHTML += '<div class="user">\n\t\t\t\t<div class="thumbnail"></div>\n\t\t\t\t<div class="chat-wrapper">' + this.message + "</div>\n\t\t\t</div>", this.message = "", document.querySelector("#chatbot-body").scrollTop = document.querySelector("#chatbot-body").scrollHeight
+					}
+				}, filters: {}, components: {}
+			}, u = s, l = (n("3021"), n("2877")), d = Object(l["a"])(u, c, i, !1, null, null, null), p = d.exports,
+			f = {name: "app", components: {SSAFYChatBot: p}}, h = f,
+			v = (n("034f"), Object(l["a"])(h, o, a, !1, null, null, null)), b = v.exports, m = n("bc3a"), y = n.n(m);
+		r["a"].config.productionTip = !1, r["a"].prototype.$http = y.a, new r["a"]({
+			render: function (t) {
+				return t(b)
+			}
+		}).$mount("#app")
+	}, "64a9": function (t, e, n) {
+	}, "94f0": function (t, e, n) {
+	}
+});
+//# sourceMappingURL=app.dc5df974.js.map
